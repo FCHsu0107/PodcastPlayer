a6e40cb37ada4343c58cf57fc556d7b56fcbd689
Jacuqueline_Hsu <hsufuchiung@gmail.com>
2020-10-23 00:17:20 +0800

    Establish Home page UI

---
+++ PodcastPlayer/View/HomePageTableViewCell.swift
@@ -0,0 +1,63 @@
+//
+//  HomePageTableViewCell.swift
+//  PodcastPlayer
+//
+//  Created by Fu-Chiung HSU on 2020/10/22.
+//
+
+import UIKit
+
+import SnapKit
+
+class HomePageTableViewCell: UITableViewCell {
+    private let podcastImage = UIImageView()
+    private let titleLabel = UILabel()
+    private let publicDateLabel = UILabel()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        setUpUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpUI() {
+        contentView.addSubview(podcastImage)
+        podcastImage.backgroundColor = .yellow
+        podcastImage.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.width.equalTo(80)
+            make.height.equalTo(podcastImage.snp.width)
+            make.leading.equalToSuperview().offset(12)
+        }
+        
+        contentView.addSubview(titleLabel)
+        titleLabel.textAlignment = .left
+        titleLabel.font = UIFont.systemFont(ofSize: 14)
+        titleLabel.textColor = .black
+        titleLabel.numberOfLines = 0
+        titleLabel.snp.makeConstraints { make in
+            make.leading.equalTo(podcastImage.snp.trailing).offset(12)
+            make.top.equalTo(podcastImage).offset(2)
+            make.trailing.equalToSuperview().offset(-12)
+        }
+        
+        contentView.addSubview(publicDateLabel)
+        publicDateLabel.textAlignment = .left
+        publicDateLabel.font = UIFont.systemFont(ofSize: 14)
+        publicDateLabel.textColor = .black
+        publicDateLabel.snp.makeConstraints { make in
+            make.leading.equalTo(titleLabel)
+            make.bottom.equalTo(podcastImage).offset(-2)
+            make.trailing.equalToSuperview().offset(-12)
+        }
+    }
+    
+    func cofigure(_ item: String) {
+        podcastImage.backgroundColor = .red
+        titleLabel.text = item + " - 賺錢光靠自我成長不夠，也要觀察外部的趨勢變化。辛苦賺來的錢幣"
+        publicDateLabel.text = "2020/10/22"
+    }
+}
--- PodcastPlayer/ViewController/HomeViewController.swift
+++ PodcastPlayer/ViewController/HomeViewController.swift
@@ -7,9 +7,44 @@
 
 import UIKit
 
+import SnapKit
+
 class HomeViewController: UIViewController {
+    
+    var tableView: UITableView!
+    var podcastList: [String] = ["A", "B", "C", "D"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpUI()
+    }
+    
+    private func setUpUI() {
+        tableView = UITableView(frame: .zero, style: .plain)
+        
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        tableView.register(cellType: HomePageTableViewCell.self)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return podcastList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(HomePageTableViewCell.self, for: indexPath)
+        let item = podcastList[indexPath.row]
+        cell.cofigure(item)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
     }
 }
