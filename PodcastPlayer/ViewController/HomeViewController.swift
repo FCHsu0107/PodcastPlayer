92ceae08f80a43c4d94e8da2daf6175664ce2a61
Jacuqueline_Hsu <hsufuchiung@gmail.com>
2020-10-22 23:27:25 +0800

    Set up main window without storyboard

--- PodcastPlayer/ViewController/HomeViewController.swift
+++ PodcastPlayer/ViewController/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  PodcastPlayer
 //
 //  Created by Fu-Chiung HSU on 2020/10/22.
@@ -7,13 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
