4c739983741bf2fb0830e1a1767c988d233691f6
Jacuqueline_Hsu <hsufuchiung@gmail.com>
2020-10-22 22:31:57 +0800

    Create project and set up git

---
+++ PodcastPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PodcastPlayer
+//
+//  Created by Fu-Chiung HSU on 2020/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
